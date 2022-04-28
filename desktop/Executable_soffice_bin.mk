fbaae0d262e177a156b2b712d59d92df2e09e885
TheRock Builder <libo@therock>
2022-04-28 14:19:22 +0000

    Add more functions to export for LOWA

--- desktop/Executable_soffice_bin.mk
+++ desktop/Executable_soffice_bin.mk
@@ -50,7 +50,7 @@ endif
 ifeq ($(OS),EMSCRIPTEN)
 
 $(eval $(call gb_Executable_add_ldflags,soffice_bin,\
-    -s EXPORTED_FUNCTIONS=["_main"$(COMMA)"_libreofficekit_hook"$(COMMA)"_libreofficekit_hook_2"$(COMMA)"_lok_preinit"$(COMMA)"_lok_preinit_2"] \
+	-s EXPORTED_FUNCTIONS=["_main"$(COMMA)"_libreofficekit_hook"$(COMMA)"_libreofficekit_hook_2"$(COMMA)"_lok_preinit"$(COMMA)"_lok_preinit_2"$(COMMA)"_doc_postUnoCommand"] \
 ))
 
 endif
