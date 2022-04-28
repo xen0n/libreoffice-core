a8735c325237003c3087fe6a094958651bf884ec
TheRock Builder <libo@therock>
2022-04-28 14:19:46 +0000

    Hack lokit API to be useable from JS
    So LOWA users/embeddings can call uno slots

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -1032,7 +1032,7 @@ static void doc_postWindowGestureEvent(LibreOfficeKitDocument* pThis,
                                       int nX,
                                       int nY,
                                       int nOffset);
-static void doc_postUnoCommand(LibreOfficeKitDocument* pThis,
+void doc_postUnoCommand(LibreOfficeKitDocument* pThis,
                                const char* pCommand,
                                const char* pArguments,
                                bool bNotifyWhenFinished);
@@ -4150,7 +4150,7 @@ static void lo_setOption(LibreOfficeKit* /*pThis*/, const char *pOption, const c
     }
 }
 
-static void doc_postUnoCommand(LibreOfficeKitDocument* pThis, const char* pCommand, const char* pArguments, bool bNotifyWhenFinished)
+void doc_postUnoCommand(LibreOfficeKitDocument* pThis, const char* pCommand, const char* pArguments, bool bNotifyWhenFinished)
 {
     comphelper::ProfileZone aZone("doc_postUnoCommand");
 
@@ -6916,9 +6916,19 @@ LibreOfficeKit *libreofficekit_hook_2(const char* install_path, const char* user
 }
 
 SAL_JNI_EXPORT
-LibreOfficeKit *libreofficekit_hook(const char* install_path)
+LibreOfficeKitDocument *libreofficekit_hook(const char*)
 {
-    return libreofficekit_hook_2(install_path, nullptr);
+    if (!gImpl)
+    {
+        gImpl = new LibLibreOffice_Impl();
+        xContext.set( ::comphelper::getProcessComponentContext(), uno::UNO_SET_THROW );    
+    }
+
+    uno::Reference<frame::XDesktop> xDesktop = frame::Desktop::create(xContext);
+    uno::Reference<lang::XComponent> xComponent = xDesktop->getCurrentComponent();
+    LibLODocument_Impl* pDocument = new LibLODocument_Impl(xComponent, 0);
+    
+    return pDocument;
 }
 
 SAL_JNI_EXPORT
